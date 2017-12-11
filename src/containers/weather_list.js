17d50913c2d0e906d3a56ffe013059d552b4f96c
j620656786206 <j620656786206@gmail.com>
2017-12-11 12:52:30 -0600

    62. Mapping Props to a Render Helper

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+
+        return (
+            <tr key={ name }>
+                <td>{ name }</td>
+            </tr>
+        );
+    }
+
     render () {
         return (
             <table className="table table-hover">
@@ -14,6 +24,7 @@ class WeatherList extends Component {
                     </tr>
                 </thead>
                 <tbody>
+                    { this.props.weather.map(this.renderWeather) }
                 </tbody>
             </table>
         );
@@ -24,4 +35,4 @@ function mapStateToProps({ weather }) {
     return { weather }; 
 }
 
-export default connect(mapStateToProps) (WeatherList) ;
+export default connect(mapStateToProps) (WeatherList);
