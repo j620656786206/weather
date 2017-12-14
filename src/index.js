276fc55906d9e6b33ecba36eb01cbcbda5c92707
Alex Yu <j620656786206@gmail.com>
2017-12-13 18:55:17 -0600

    deploy to heroku - changed package.json webapckconfigs ./src/index

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import ReduxPromise from 'redux-promise';
 import App from './components/app';
 import reducers from './reducers';
 
+if (process.env.NODE_ENV !== 'production') {
+     console.log('Looks like we are in development mode!');
+}
+
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
