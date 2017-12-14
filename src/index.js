125c11539741d542d7de688f2551e28c2d4235a4
Alex Yu <j620656786206@gmail.com>
2017-12-13 21:02:52 -0600

    deploy to heroku

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import ReduxPromise from 'redux-promise';
 import App from './components/app';
 import reducers from './reducers';
 
-if (process.env.NODE_ENV !== 'production') {
-     console.log('Looks like we are in development mode!');
-}
-
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
