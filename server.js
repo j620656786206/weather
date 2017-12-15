f1f3c067845fb6a5c4d81a074bd844cda2223bd8
Alex Yu <j620656786206@gmail.com>
2017-12-14 22:28:42 -0600

    deplot to heroku

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port);
 
-console.log("Server started");
+console.log('Server started');
