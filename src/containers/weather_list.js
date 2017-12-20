9a94077abfa6c6ceb382250e64cb7fd5a8ee1d21
Alex Yu <j620656786206@gmail.com>
2017-12-19 20:51:36 -0600

    clear some sytax errors from eslint

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -12,30 +12,30 @@ class WeatherList extends Component {
     const {lon, lat} = cityData.city.coord;
 
     return (
-        <tr key={ name }>
-            <td><GoogleMap lon={lon} lat={lat} /></td>
-            <td><Chart data={temps} color="orange" units="K" /></td>
-            <td><Chart data={pressures} color="green" units="hPa" /></td>
-            <td><Chart data={humidities} color="black" units="%" /></td>
-        </tr>
+      <tr key={ name }>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
+      </tr>
     );
   }
 
   render() {
     return (
-        <table className="table table-hover">
-            <thead>
-                <tr>
-                <th>City</th>
-                <th>Temperature (K)</th>
-                <th>Pressure (hPa)</th>
-                <th>Humidity (%)</th>
-                </tr>
-            </thead>
-            <tbody>
-                { this.props.weather.map(this.renderWeather) }
-            </tbody>
-        </table>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.props.weather.map(this.renderWeather) }
+        </tbody>
+      </table>
     );
   }
 }
